90a35eedf237625d10a524b54413698292a06e7a
RMRattray <rmr3141@gmail.com>
2024-08-26 09:32:34 -0400

    How did this get left out?

---
+++ filestr.h
@@ -0,0 +1,10 @@
+// ***
+// *** DO NOT modify this file
+// ***
+
+#ifndef FILESTR_H
+#define FILESTR_H
+#include <stdio.h>
+int totalWordCount(char * filename, int max_word_length);
+void eachWordCount(char * filename, int max_word_length, int total_word_count);
+#endif
